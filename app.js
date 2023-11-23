d1c2da0d65844db00aca007645b33c1658cce484
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-11-23 17:06:47 +0330

    Creating products classes and styling

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import Products from "./models/Products.js";
 import fetchData from "./utils/httpReq.js";
-
-const render = async () => {
+const productsNode = document.getElementById("products");
+async function render() {
   const productData = await fetchData();
-};
+  const productsInstance = new Products(productsNode, productData);
+  productsInstance.showProducts();
+}
 
 document.addEventListener("DOMContentLoaded", render);
---
+++ models/Products.js
@@ -0,0 +1,51 @@
+class Products {
+  constructor(parent, products) {
+    this.parent = parent;
+    this.products = products;
+    this.parent.addEventListener("click", this);
+  }
+
+  showProducts() {
+    this.products.forEach((product) => {
+      this.createCard(product);
+    });
+  }
+  createCard(data) {
+    const cardEle = document.createElement("div");
+    const imgEle = this.productImg(data);
+    const infoEle = this.productInfo(data);
+    cardEle.innerHTML = imgEle;
+    cardEle.innerHTML += infoEle;
+
+    this.parent.appendChild(cardEle);
+  }
+  productImg(data) {
+    const { image, alt } = data;
+    const imgJSX = `<img alt=${alt} src=${image}>`;
+    return imgJSX;
+  }
+  productInfo(data) {
+    const { id, name, price } = data;
+    const infoJSX = `
+    <div id="product-info">
+    <h3>${name}</h3>
+    <div>
+    <span>${price}</span>
+    <button data-id=${id}>+</button>
+    </div>
+    </div>
+    `;
+    return infoJSX;
+  }
+  handleEvent() {
+    const element = event.target;
+    if (element.tagName === "BUTTON") {
+      this.addToCart(element.dataset.id);
+    }
+  }
+  addToCart(id) {
+    console.log(id);
+  }
+}
+
+export default Products;
